db5fa7864db71c96068166b84772e169acdde9cd
ManikantaGopisetti <manikantagopisetti2@gmail.com>
2021-12-03 15:17:48 +0530

    ADDED: PrimeNumbers program to repo

--- src/com/bridgelabz/algorithms/Main.java
+++ src/com/bridgelabz/algorithms/Main.java
@@ -4,10 +4,9 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		String str1="mani";
-		String str2="anil";
-		Anagram anagram=new Anagram();
-		System.out.println("Given strings are Anagram : "+anagram.anagramDetect(str1, str2));
+		int range=1000;
+		PrimeNumbers primeNumbers = new PrimeNumbers();
+		primeNumbers.isPrimeNum(range);
 	}
 
 }
